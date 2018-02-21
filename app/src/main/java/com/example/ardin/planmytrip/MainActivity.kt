eadcf1a778ade8fc0241398735c5b8974f54578f
Muhtarudin Siregar ツ <redcar.studious@gmail.com>
2018-02-21 10:08:43 +0700

    Styling

--- app/src/main/java/com/example/ardin/planmytrip/MainActivity.kt
+++ app/src/main/java/com/example/ardin/planmytrip/MainActivity.kt
@@ -2,12 +2,11 @@ package com.example.ardin.planmytrip
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.example.ardin.planmytrip.R.layout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(layout.activity_main)
+        setContentView(R.layout.activity_main)
     }
 }
