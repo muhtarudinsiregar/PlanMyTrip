052f98f2a2041742763933a30db405e17ea88a6c
Muhtarudin Siregar ツ <redcar.studious@gmail.com>
2018-02-22 15:27:21 +0700

    Add back button in signup page

--- app/src/main/java/com/example/ardin/planmytrip/feature/SignupActivity.kt
+++ app/src/main/java/com/example/ardin/planmytrip/feature/SignupActivity.kt
@@ -2,8 +2,6 @@ package com.example.ardin.planmytrip.feature
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.widget.Toast
 import com.example.ardin.planmytrip.R
 import com.example.ardin.planmytrip.db.UserDbHelper
 import com.example.ardin.planmytrip.model.UserModel
@@ -15,9 +13,16 @@ import kotlinx.android.synthetic.main.activity_signup.*
 class SignupActivity : AppCompatActivity() {
     private lateinit var userDbHelper: UserDbHelper
 
+    override fun onNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
 
         userDbHelper = UserDbHelper(this)
 
